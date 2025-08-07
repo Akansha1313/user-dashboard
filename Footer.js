1286e6ea76ae3cff468046d87d14a1dc043f14b1
Akansha1313 <ak.akanshamishra@gmail.com>
2025-08-07 12:10:04 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        <div>© 2025 . All rights reserved.</div>
+        <div className="footer-links">
+          <a href="#">Privacy Policy</a> | <a href="#">Terms</a> | <a href="#">Support</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+
+export default Footer;
